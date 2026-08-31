fddb0ddb767e47d2bc876c9eb3487bce0c8f8b85
piterssss190 <piterssss1900@gmail.com>
2026-08-31 03:01:19 +0200

    Add files via upload

--- src/main/java/com/dragonmod/client/ModMainClient.java
+++ src/main/java/com/dragonmod/client/ModMainClient.java
@@ -6,11 +6,12 @@ import com.dragonmod.network.ModNetworking;
 import com.mojang.blaze3d.platform.InputConstants;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.ModelLayerRegistry;
 import net.minecraft.client.KeyMapping;
+import net.minecraft.resources.Identifier;
 import org.lwjgl.glfw.GLFW;
 
 /**
@@ -31,11 +32,16 @@ import org.lwjgl.glfw.GLFW;
  */
 public class ModMainClient implements ClientModInitializer {
 
-    private static final KeyMapping DESCEND_KEY = KeyBindingHelper.registerKeyBinding(new KeyMapping(
+    // UWAGA MAPPINGI: od Fabric API 26.1 kategoria klawisza to obiekt
+    // KeyMapping.Category (nie zwykły String jak w starszych wersjach).
+    private static final KeyMapping.Category CATEGORY =
+            new KeyMapping.Category(Identifier.fromNamespaceAndPath("dragonmod", "dragonmod"));
+
+    private static final KeyMapping DESCEND_KEY = KeyMappingHelper.registerKeyMapping(new KeyMapping(
             "key.dragonmod.descend",
             InputConstants.Type.KEYSYM,
             GLFW.GLFW_KEY_LEFT_CONTROL,
-            "key.categories.dragonmod"
+            CATEGORY
     ));
 
     @Override
